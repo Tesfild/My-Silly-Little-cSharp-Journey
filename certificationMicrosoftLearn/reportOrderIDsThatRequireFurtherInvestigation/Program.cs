fa613c1508fd883aa013de9d569e2721650240e0
Rebek <160554014+Tesfild@users.noreply.github.com>
2025-07-13 01:12:49 -0300

    Add files via upload

---
+++ certificationMicrosoftLearn/reportOrderIDsThatRequireFurtherInvestigation/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] suspectIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach(string id in suspectIds) {
+
+    if ( id.StartsWith("B")) {
+        Console.WriteLine($"The Id: '{id}' starts with an 'B'.");
+    }
+}
